3bc140873ca8be92930ce9d95e71924f737815f2
Shimadako <rogepalma99@hotmail.com>
2025-05-29 11:02:50 -0600

    Actualiza Clases para el write y read

--- src/com/tecnm/itics/proyecto/oxxo/Main.java
+++ src/com/tecnm/itics/proyecto/oxxo/Main.java
@@ -1,9 +1,11 @@
 package com.tecnm.itics.proyecto.oxxo;
 
+import java.io.IOException;
+
 public class Main {
-
+    public static void main (String[] args) throws IOException {
     Personal trab1 = new Personal(1001, "Rogelio", "Zepeda", true);
-    ObjectsFileHandler.
-
+    ObjectsFileHandler.writeBinaryFile("User", trab1);
+    }
 
 }

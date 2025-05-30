d448d22e212987d05d249c42cb5415e4fda265ea
Shimadako <rogepalma99@hotmail.com>
2025-05-30 10:30:00 -0600

    Actualiza Clases para el write y read

--- src/com/tecnm/itics/proyecto/oxxo/Main.java
+++ src/com/tecnm/itics/proyecto/oxxo/Main.java
@@ -1,11 +1,19 @@
 package com.tecnm.itics.proyecto.oxxo;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main (String[] args) throws IOException {
-    Personal trab1 = new Personal(1001, "Rogelio", "Zepeda", true);
-    ObjectsFileHandler.writeBinaryFile("C:\\Users\\rogep\\Desktop\\Escuela\\prueba.dat", trab1);
+        Personal trab1 = new Personal(1001, "Rogelio", "Zepeda", true);
+        //Escribir Texto
+        FileHandler.writeTextFile("C:\\Users\\rogep\\Desktop\\Escuela\\texto.txt", List.of("1001", "Rogelio", "Zepeda", "true"));
+        //Leer Texto
+        List<String> logEngtries = FileHandler.readTextFile("C:\\Users\\rogep\\Desktop\\Escuela\\texto.txt");
+        //Escribir binario
+        ObjectsFileHandler.writeBinaryFile("C:\\Users\\rogep\\Desktop\\Escuela\\prueba.dat", trab1);
+        //Leer binario
+        byte[] imageData = FileHandler.readBinaryFile("C:\\Users\\rogep\\Desktop\\Escuela\\prueba.dat");
     }
 
 }

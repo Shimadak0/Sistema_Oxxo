b78eabcd43194535891124d8926dd8a0a69bbf1d
Shimadako <rogepalma99@hotmail.com>
2025-05-29 18:28:20 -0600

    para ejercicio de clase

--- src/com/tecnm/itics/proyecto/oxxo/Main.java
+++ src/com/tecnm/itics/proyecto/oxxo/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
     public static void main (String[] args) throws IOException {
     Personal trab1 = new Personal(1001, "Rogelio", "Zepeda", true);
-    ObjectsFileHandler.writeBinaryFile("User", trab1);
+    ObjectsFileHandler.writeBinaryFile("C:\\Users\\rogep\\Desktop\\Escuela\\prueba.dat", trab1);
     }
 
 }
